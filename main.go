1627c4082b393e4c48025af3609323502bb8772c
Sahej Singh <sahej.singh@zopsmart.com>
2022-11-20 17:44:10 +0530

    setup api calls for get, put, delete by id

--- main.go
+++ main.go
@@ -2,50 +2,113 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"io"
 	"movieApi/models"
 	"net/http"
+	"strconv"
 )
 
 func main() {
-	fmt.Println(models.GetMovie(false))
-	models.UpdateMovie([]models.MovieModel{
-		{
-			1,
-			"Breaking bad",
-			"Drama",
-			9.9,
-			"Chemistry professor breaks bad",
-			true,
-		},
-	})
+	setupServer()
+}
+
+func setupServer() {
 	muxRouter := mux.NewRouter().StrictSlash(true)
-	muxRouter.HandleFunc("/", handleStuff)
-	muxRouter.HandleFunc("/update", handleUpdate)
+	muxRouter.HandleFunc("/movies", handleMovies).Methods("GET", "POST")
+	muxRouter.HandleFunc("/movie/{id}", handleMovieById).Methods("GET", "PUT", "DELETE")
 	http.ListenAndServe(":8080", muxRouter)
 }
 
-func handleUpdate(writer http.ResponseWriter, request *http.Request) {
-	if request.Method == "POST" {
-		newData, err := io.ReadAll(request.Body)
-		if err != nil {
-			panic(err)
+func handleMovies(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "GET":
+		{
+			getMovies(writer)
 		}
-		var updatedStruct []models.MovieModel
-		json.Unmarshal(newData, &updatedStruct)
-		models.UpdateMovie(updatedStruct)
-	} else {
-		writer.WriteHeader(405)
-		writer.Write([]byte("Method not allowed"))
+	case "POST":
+		{
+			postMovies(request)
+		}
 	}
 }
 
-func handleStuff(writer http.ResponseWriter, request *http.Request) {
-	dataToShow, err := json.Marshal(models.GetMovie(false))
+func getMovies(writer http.ResponseWriter) {
+	dataToShow, err := json.Marshal(models.GetMovies())
 	if err != nil {
 		panic(err)
 	}
 	writer.Write(dataToShow)
+}
+
+func postMovies(request *http.Request) {
+	newData, err := io.ReadAll(request.Body)
+	if err != nil {
+		panic(err)
+	}
+	var updatedStruct []models.MovieModel
+	json.Unmarshal(newData, &updatedStruct)
+	models.UpdateMovies(updatedStruct)
+}
+
+func handleMovieById(writer http.ResponseWriter, request *http.Request) {
+	var id = 0
+	for _, val := range mux.Vars(request) {
+		i, err := strconv.Atoi(val)
+		if err != nil {
+			panic(err)
+		}
+		id = i
+	}
+
+	switch request.Method {
+	case "GET":
+		{
+			getMovieById(id, writer)
+		}
+	case "PUT":
+		{
+			putMovieById(writer, request.Body)
+		}
+	case "DELETE":
+		{
+			deleteMovieById(id, writer)
+		}
+	}
+
+}
+
+func getMovieById(id int, writer http.ResponseWriter) {
+	movie := models.GetMovieById(id)
+	movieJson, err := json.Marshal(movie)
+	if err != nil {
+		panic(err)
+	}
+	writer.Write(movieJson)
+}
+
+func deleteMovieById(id int, writer http.ResponseWriter) {
+	movieMap := models.ReadMovies()
+	_, itExists := movieMap[id]
+	if itExists {
+		models.DeleteMovieById(id)
+	} else {
+		writer.Write([]byte("Movie with this id does not exist"))
+		writer.WriteHeader(405)
+	}
+}
+
+func putMovieById(writer http.ResponseWriter, body io.ReadCloser) {
+	movieJson, err := io.ReadAll(body)
+	if err != nil {
+		panic(err)
+	}
+	var movieStruct models.MovieModel
+	err = json.Unmarshal(movieJson, &movieStruct)
+	if err != nil {
+		writer.Write([]byte("Bad request"))
+		writer.WriteHeader(405)
+		return
+	}
+	models.PutMovieById(movieStruct)
 }
--- models/movieModel.go
+++ models/movieModel.go
@@ -15,14 +15,29 @@ type MovieModel struct {
 	Released bool
 }
 
-func GetMovie(isUpdated bool) (moviesList []MovieModel) {
+func ReadMovies() (mapOfMovies map[int]MovieModel) {
+	mapOfMovies = map[int]MovieModel{}
+	jsonData, err := os.ReadFile("./data/MoviesList.json")
+	if err != nil {
+		panic(err)
+	}
+	var movies []MovieModel
+	err = json.Unmarshal(jsonData, &movies)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, movie := range movies {
+		mapOfMovies[(movie.Id)] = movie
+	}
+
+	return
+}
+
+func GetMovies() (moviesList []MovieModel) {
 	var fileData []byte
 	var err = error(nil)
-	if isUpdated {
-		fileData, err = os.ReadFile("./data/UpdatedMoviesList.json")
-	} else {
-		fileData, err = os.ReadFile("./data/MoviesList.json")
-	}
+	fileData, err = os.ReadFile("./data/MoviesList.json")
 
 	if err != nil {
 		panic(err)
@@ -39,13 +54,41 @@ func GetMovie(isUpdated bool) (moviesList []MovieModel) {
 	return moviesList
 }
 
-func UpdateMovie(data []MovieModel) {
+func UpdateMovies(data []MovieModel) {
 	jsonData, err := json.Marshal(data)
 	if err != nil {
 		panic(err)
 	}
-	err = os.WriteFile("./data/UpdatedMoviesList.json", jsonData, 0644)
+	err = os.WriteFile("./data/MoviesList.json", jsonData, 0644)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func GetMovieById(id int) (movie MovieModel) {
+	mapOfMovies := ReadMovies()
+
+	return mapOfMovies[id]
+}
+
+func DeleteMovieById(id int) {
+	mapOfMovies := ReadMovies()
+	delete(mapOfMovies, id)
+	var movies []MovieModel
+	for _, movie := range mapOfMovies {
+		movies = append(movies, movie)
+	}
+
+	UpdateMovies(movies)
+}
+
+func PutMovieById(movieStruct MovieModel) {
+	mapOfMovies := ReadMovies()
+
+	var movies []MovieModel
+	movies = append(movies, movieStruct)
+	for _, movie := range mapOfMovies {
+		movies = append(movies, movie)
+	}
+	UpdateMovies(movies)
 }
