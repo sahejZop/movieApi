a86c64ad29f538943e40b0e499f8cd8f48634f69
Sahej Singh <sahej.singh@zopsmart.com>
2022-11-20 16:10:35 +0530

    used gorilla mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module movieApi
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"io"
 	"movieApi/models"
+	"net/http"
 )
 
 func main() {
@@ -17,5 +21,31 @@ func main() {
 			true,
 		},
 	})
-	fmt.Println(models.GetMovie(true))
+	muxRouter := mux.NewRouter().StrictSlash(true)
+	muxRouter.HandleFunc("/", handleStuff)
+	muxRouter.HandleFunc("/update", handleUpdate)
+	http.ListenAndServe(":8080", muxRouter)
+}
+
+func handleUpdate(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" {
+		newData, err := io.ReadAll(request.Body)
+		if err != nil {
+			panic(err)
+		}
+		var updatedStruct []models.MovieModel
+		json.Unmarshal(newData, &updatedStruct)
+		models.UpdateMovie(updatedStruct)
+	} else {
+		writer.WriteHeader(405)
+		writer.Write([]byte("Method not allowed"))
+	}
+}
+
+func handleStuff(writer http.ResponseWriter, request *http.Request) {
+	dataToShow, err := json.Marshal(models.GetMovie(false))
+	if err != nil {
+		panic(err)
+	}
+	writer.Write(dataToShow)
 }
